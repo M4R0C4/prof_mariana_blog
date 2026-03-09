9222d3a3e027ce0fa8157a36afd300d48a4a5952
Mariana Rodrigues <mariana.rodrigues.or@gmail.com>
2026-03-09 08:13:42 -0300

    Create User model #5

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,17 +5,20 @@ from flasgger import Swagger
 from flask_cors import CORS
 from .config import Config
 
+
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
     Swagger(app)
     CORS(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
+
 
     from .routes.article_routes import article_bp
     app.register_blueprint(article_bp)
